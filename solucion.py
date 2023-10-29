d44aed29fd4aa4030d0a6a19f0bcf40bae94f53f
nubol23 <rafael.rvp98@gmail.com>
2023-10-29 00:50:59 -0400

    Setup lab 2.1

---
+++ solucion.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+
+def ej_1_cargar_csv() -> pd.DataFrame:
+    pass
+
+
+def ej_2_convertir_fecha(df: pd.DataFrame) -> None:
+    # TODO: debes cambiar el valor de la colúmna de forma inplace
+    # es decir, sobre el mismo dataframe, no creando uno nuevo
+    pass
+
+
+def ej_3_convertir_cantidad(df: pd.DataFrame) -> None:
+    # TODO: debes cambiar el valor de la colúmna de forma inplace
+    # es decir, sobre el mismo dataframe, no creando uno nuevo
+    pass
+
+
+def ej_4_fecha_como_indice(df: pd.DataFrame) -> None:
+    # TODO: debes cambiar el valor de la colúmna de forma inplace
+    # es decir, sobre el mismo dataframe, no creando uno nuevo
+    pass
+
+
+def ej_5_filtrar_por_fecha(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+def ej_6_filtrar_por_producto(df: pd.DataFrame) -> pd.DataFrame:
+    pass
